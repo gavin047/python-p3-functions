cecb2742076f5d07253f9202c01f6f1f709fe757
gavin <gavinmichori@gmail.com>
2024-08-29 11:45:27 +0300

    passed

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
+ 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+greet("Naureen")
+
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default("Sunny")
 
 def add(num1, num2):
-    pass
+    function_name = num1 + num2
+    return function_name    
+
+print(add(1, 2))
+
 
 def halve(number):
-    pass
+    return number / 2
+print(halve(100))
